044400d811c648476c86a5cfffb49001d96dbca0
amlan564 <amlanroy4396@gmail.com>
2025-05-02 17:33:27 +0600

    first commit

---
+++ app/data/data.ts
@@ -0,0 +1,6 @@
+ export const baseInfo = {
+  name: "Amlan Chakraborty",
+  position: "Full Stack Web Developer",
+  description: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  profileImage: "/images/profile.jpeg",
+};
---
+++ constant/constant.ts
@@ -0,0 +1,24 @@
+import { url } from "inspector";
+
+export const navLinks = [
+    {
+        id: 1,
+        url: "#",
+        label: "Home",
+    },
+    {
+        id: 2,
+        url: "#",
+        label: "About",
+    },
+    {
+        id: 3,
+        url: "#",
+        label: "Home",
+    },
+    {
+        id: 1,
+        url: "#",
+        label: "Home",
+    },
+]
