dc104d0c9c0944f14d1da18845f26ebd3f985e7b
amlan564 <amlanroy4396@gmail.com>
2025-05-15 17:23:43 +0600

    about section updated

--- app/components/Home/About/About.tsx
+++ app/components/Home/About/About.tsx
@@ -8,11 +8,11 @@ const About = () => {
     <div className="pt-20 pb-20 bg-gray-50" id="about">
       {/* Section Heading */}
       <SectionHeading>About Me</SectionHeading>
-      <div className="w-[60%] mx-auto grid grid-cols-1 lg:grid-cols-3 gap-20 items-center mt-20">
+      <div className="w-[60%] md:w-[80%] lg:w-[80%] xl:w-[60%] mx-auto grid grid-cols-1 lg:grid-cols-3 gap-20 items-center mt-20">
         {aboutInfo.map((item) => (
           <div
             key={item.id}
-            className="w-[220px] md:w-full flex flex-col items-center text-center h-full transition-all duration-500 ease-in-out hover:scale-110"
+            className="w-full flex flex-col items-center text-center h-full transition-all duration-500 ease-in-out hover:scale-110"
           >
             <Image src={item.img} alt="" width={300} height={20} className="md:h-40 object-cover rounded-md mb-6"/>
             <h2 className="text-md font-semibold mb-2">
